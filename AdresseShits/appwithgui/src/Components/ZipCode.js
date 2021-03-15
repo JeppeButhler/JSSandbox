3509324808f4d6d008efa2d38a3e88c42bbaf384
Jeppe Buthler <JeppeButhler@hotmail.com>
2021-03-15 12:58:52 +0100

    Init AdresseGUI

---
+++ AdresseShits/appwithgui/src/Components/ZipCode.js
@@ -0,0 +1,29 @@
+const ZipCode = () => {
+    const lookUpZipCode = (zipcode) => {
+        //let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+        let xhr = new XMLHttpRequest();
+        let uri = `https://dawa.aws.dk/postnumre/${zipcode}`;
+
+        xhr.open("GET", uri);
+        xhr.setRequestHeader("Accept", "application/json");
+        await xhr.onreadystatechange;
+        let response = JSON.parse(xhr.responseText);
+        document.getElementById("cityName").value = response["navn"];
+    };
+
+    const onClick = () => {
+        let zipCodeFromInput = document.getElementById("myZip").value;
+        return lookUpZipCode(zipCodeFromInput);
+    }
+        
+    return (
+        
+        <div>
+            <input id='myZip' type='number' placeholder='Indtast postnummer her'/>
+            <p id='cityName' value=''/>
+            <button type='submit' onClick='onClick' value='Find bynavn'/>
+        </div>
+    )
+}
+
+export default ZipCode
