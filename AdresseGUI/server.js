538264b5b178bbe35e18bfb97e87d713790f9040
Jeppe Buthler <JeppeButhler@hotmail.com>
2021-08-15 22:47:42 +0200

    Late summer cleaning. Having troubles deleting failed project.

--- AdresseGUI/server.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-var http = require('http');
-var port = 4000;
-
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end('Hello World\n');
-}).listen(port);
-
