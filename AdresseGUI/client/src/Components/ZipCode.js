543f9b843e6ae0afe635edc0dcffc2f75eb20231
Jeppe Buthler <JeppeButhler@hotmail.com>
2021-08-16 21:05:41 +0200

    More cleaning. Hopefully all good now.

--- AdresseGUI/client/src/Components/ZipCode.js
+++ AdresseGUI/client/src/Components/ZipCode.js
@@ -1,27 +1,45 @@
 const ZipCode = () => {
 
-    const performLookup = (zipcode, callback) => {
-        fetch(`http://localhost:9000/zip?zipcode=${zipcode}`)
-            .then(res => res.json())
-                .then((jsonData) => {
-                    callback(jsonData);
-                    console.log(jsonData);
-                });
+    const performLookup = (zipcode) => {
+        let uri = `https://api.dataforsyningen.dk/postnumre/${zipcode}`;
+    let requestOptions = {
+        method: "GET",
+        responseType: "application/json",
+        mode: 'cors'
+    };
+
+    fetch(uri, requestOptions)
+        .then(data => {
+            if(data.ok) {
+                return data.json();
+        }}) 
+        .then(jsonData => {
+            return jsonData.navn;
+        })
+        .then(navn => {
+            document.getElementById("resultParagraph").innerText = navn;
+        })
+        .catch(err => {
+            document.getElementById("resultParagraph").innerText = "En fejl opstod. Prøv igen.";
+        });
     };
 
     const readInputAndPassRequest = () => {
         let zipcode = document.getElementById("zipCodeInput").value;
-        performLookup(zipcode, (result) => {
-            document.getElementById("resultParagraph").textContent = result.city;
-        });
+        if(zipcode >= 1000 && zipcode < 10000) {
+            performLookup(zipcode);
+        } else {
+            document.getElementById("resultParagraph").innerText = "Et postnummer indeholder fire cifre."
+        }
     }
 
     return (
         <div>
-            <p id="resultParagraph"></p>
-                <label>Enter zipcode to lookup.</label>
-                <input id="zipCodeInput" type="number"/>
-                <button onClick={readInputAndPassRequest}/>
+            <div id="resultDiv">
+                <input id="zipCodeInput" type="number" placeholder="Enter zipcode"/>
+            </div>
+            <button onClick={readInputAndPassRequest}>Search</button> 
+            <p id="resultParagraph"/>
         </div>
     )
 }
